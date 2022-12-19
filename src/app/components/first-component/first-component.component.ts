26111b45da4db80ced792013197d60e30dd455b3
luizfelper <luizfelper@gmail.com>
2022-12-19 14:10:19 -0300

    Css style example

--- src/app/components/first-component/first-component.component.ts
+++ src/app/components/first-component/first-component.component.ts
@@ -1,23 +1,30 @@
 import { Component } from '@angular/core';
 
+type seguidoresTypes = {
+  login?: string;
+};
 @Component({
   selector: 'app-first-component',
   templateUrl: './first-component.component.html',
   styleUrls: ['./first-component.component.css']
 })
+
 export class FirstComponentComponent {
 
   name: string ="Felipe";
   age: number = 30;
   languages: string[] = ["ReactJS", "JavaScript", "TypeScript", "NodeJS"];
-  seguidores: [{login: string}] = [{login: 'Felipe'}];
+  seguidores: [seguidoresTypes] = [{}];
 
-  getGithub() {
+  getGithubData(): void {
     fetch('https://api.github.com/users/luizfelper/followers').then(response => {
       return response.json();
-    }).then().then(data => {
-      console.log(data);
+    }).then(data => {
       this.seguidores = data;
     });
+  };
+
+  ngOnInit() {
+    this.getGithubData();
   };
 }
